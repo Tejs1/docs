ab0652c786f9784362a0979f058cc2a4c986d581
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2024-02-27 18:49:39 +0530

    Update lostPixelProjectId in lostpixel.config.js

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -4,7 +4,7 @@ module.exports = {
     baseUrl: 'http://172.17.0.1:9000',
     mask: [{ selector: 'span.gatsby-resp-image-background-image' }],
   },
-  lostPixelProjectId: 'clb5ek3mm1772001qqg7yban38',
+  lostPixelProjectId: 'clt4a10277si8ll0e5lz7wwue',
   apiKey: process.env.LOST_PIXEL_API_KEY,
   compareConcurrency: 10,
   shotConcurrency: 10,
